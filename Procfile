00c101e2bc5f61e23a411b72f8c8ca1148e144c6
yogendra bohara <sabiyogi19@gmail.com>
2021-04-17 10:50:35 -0700

    requirements are added and files migrated

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn tweetdemo.wsgi --log-file -
+release: python manage.py migrate
+web: gunicorn tweetdemo.wsgi
--- tweetdemo/settings.py
+++ tweetdemo/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 't2d+_i+rme@g7zx^x(b)eggup-zkkz$*i=-v(+-)%hvsd+dk&h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '*.herokuapp.com']
 
 
 # Application definition
