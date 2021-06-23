158ea6f339f341cc88071a6b0872a44458394e35
yogendra bohara <sabiyogi19@gmail.com>
2021-06-23 09:55:18 -0700

    set heroku

--- tweetdemo/settings.py
+++ tweetdemo/settings.py
@@ -29,10 +29,9 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = 't2d+_i+rme@g7zx^x(b)eggup-zkkz$*i=-v(+-)%hvsd+dk&h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['']
-# ALLOWED_HOSTS = ['127.0.0.1', '*.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '*.herokuapp.com']
 
 
 # Application definition
